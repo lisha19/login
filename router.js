26a4e45ff2bc1cb09e11be5c8c8d1c8235a58282
Lisha <lisharaj877@gmail.com>
2022-01-12 09:32:41 +0530

    lisha

---
+++ router.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+const credential={
+    email:"admin@gmail.com",
+    password:"lishanit"
+}
+
+router.post('/login',(req,res)=>{
+    if(req.body.email== credential.email && req.body.password==credential.password){
+    req.session.user == req.body.email;
+    // res.redirect('/dashboard');
+    res.end("Login hii");
+}else{
+    res.end("Invalid Username")
+}
+})
+module.exports=router;
